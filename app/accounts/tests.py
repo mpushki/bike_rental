963078a2576d52ecc738472fb02584cae6783118
maria pushkina <mariia.pushkina@gmail.com>
2024-08-06 11:19:38 +0300

    flake8

--- app/accounts/tests.py
+++ app/accounts/tests.py
@@ -1,5 +1,3 @@
-import pytest
-
 from django.urls import reverse
 
 import pytest
@@ -7,8 +5,8 @@ import pytest
 
 @pytest.mark.django_db
 def test_user_create(client):
-   data = {'password': 'someone', 'name': 'Some User', 'email':'someone@gmail.com'}
-   url = reverse('sign_up')
-   response = client.post(url, data, format='json')
-   assert response.status_code == 201
-   assert 'someone@gmail.com' == response.data.get('email')
+    data = {'password': 'someone', 'name': 'Some User', 'email': 'someone@gmail.com'}
+    url = reverse('sign_up')
+    response = client.post(url, data, format='json')
+    assert response.status_code == 201
+    assert 'someone@gmail.com' == response.data.get('email')
